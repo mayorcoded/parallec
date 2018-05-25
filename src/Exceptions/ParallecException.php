27d319eb596707d645f5eaa234229fa09edd3f1d
Mayowa <mtudonu85@gmail.com>
2018-05-25 22:04:05 +0100

    Add parallec exception class

---
+++ src/Exceptions/ParallecException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mac
+ * Date: 5/25/18
+ * Time: 9:59 PM
+ */
+
+namespace mayorcoded\parallec\Exceptions;
+
+
+class ParallecException extends \Exception
+{
+
+}

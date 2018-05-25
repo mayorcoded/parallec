23b39881bb37e57820dab4fdf520057fa2419090
Mayowa <mtudonu85@gmail.com>
2018-05-25 22:07:13 +0100

    Add exception class for handling invalid exceptions

---
+++ src/Exceptions/ParallecInvalidParameterException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mac
+ * Date: 5/25/18
+ * Time: 10:06 PM
+ */
+
+namespace mayorcoded\parallec\Exceptions;
+
+
+class ParallecInvalidParameterException extends \Exception
+{
+
+}

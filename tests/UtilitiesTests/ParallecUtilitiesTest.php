38577adef27e0a1c899c909684554c092e1da85e
Mayowa <mtudonu85@gmail.com>
2018-05-26 23:58:14 +0100

    Add test functions to test utilities functionalities

--- tests/UtilitiesTests/ParallecUtilitiesTest.php
+++ tests/UtilitiesTests/ParallecUtilitiesTest.php
@@ -5,8 +5,42 @@
  * Date: 5/26/18
  * Time: 5:35 PM
  */
+use mayorcoded\parallec\Utilities\ParallecUtilities;
+use mayorcoded\parallec\Exceptions\ParallecInvalidParameterException;
 
-class ParallecUtilitiesTest
+class ParallecUtilitiesTest extends \PHPUnit\Framework\TestCase
 {
+    public function testCurlHandlerAsResource(){
+        $this->assertInternalType('resource', ParallecUtilities::setUpCurlHandler('https://www.google.com/'));
+    }
 
+    public function testCurHandlerWithBadUrl(){
+        $this->expectException(ParallecInvalidParameterException::class);
+        ParallecUtilities::setUpCurlHandler('');
+    }
+
+    public function testUrlValidity(){
+
+        $urls = [
+            'https://www.google.com/',
+            'http://blog.teamtreehouse.com/5-new-features-php-7'
+        ];
+
+        for($i = 0; $i < sizeof($urls); $i++){
+            $this->assertTrue(ParallecUtilities::urlIsValid($urls[$i]));
+        }
+    }
+
+    public function testUrlInvalidity(){
+
+        $invalidUrls = [
+            '',
+            'adfadfd',
+            'https//google'
+        ];
+
+        for ($i = 0; $i < sizeof($invalidUrls); $i++){
+            $this->assertFalse(ParallecUtilities::urlIsValid($invalidUrls[$i]));
+        }
+    }
 }

10c29e3952e67df0baf5773b51395c2c761f9990
Mayowa <mtudonu85@gmail.com>
2018-05-26 17:43:23 +0100

    Set default parameter value to array

--- src/Utilities/ParallecUtilities.php
+++ src/Utilities/ParallecUtilities.php
@@ -12,4 +12,21 @@ namespace mayorcoded\parallec\Exceptions;
 class ParallecUtilities
 {
 
+    /**
+     * @param $url
+     * @param $parameters
+     * @return resource
+     *
+     * set up curl handler for a request with a url and  optional parameters
+     */
+    public static function setUpCurlHandler($url, $parameters = array()){
+        $curlHandler = curl_init($url);
+        curl_setopt($curlHandler, CURLOPT_RETURNTRANSFER,1);
+
+        foreach ($parameters as $parameter => $value){
+            curl_setopt($curlHandler, $parameter, $value);
+        }
+
+        return $curlHandler;
+    }
 }

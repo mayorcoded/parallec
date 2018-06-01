eea3f8a600d6e1b3965ce10b86e48148c8d4179f
Mayowa <mtudonu85@gmail.com>
2018-06-01 04:02:19 +0100

    Add magic get method

--- src/Model/ParallecCurlModel.php
+++ src/Model/ParallecCurlModel.php
@@ -6,8 +6,43 @@
  * Time: 3:36 AM
  */
 namespace mayorcoded\parallec\Model;
+use mayorcoded\parallec\Parallec
 
 class ParallecCurlModel
 {
 
+    /**
+     * This stores the resource id for a curl request
+     *
+     * @var integer
+     */
+    private $resourceId;
+
+
+    /**
+     * An instance of the Parallec class
+     *
+     * @var Parallec
+     */
+    private $parallec;
+
+
+    /**
+     * ParallecCurlModel constructor.
+     * Initialize the resource id
+     * Initialize parallec with single instance of Parallec class
+     *
+     * @param $resourceId
+     */
+    public function __construct($resourceId)
+    {
+        $this->resourceId = $resourceId;
+        $this->parallec = Parallec::getInstance();
+    }
+
+    public function __get($key)
+    {
+        $curlResponses = $this->parallec->getCurlResponses($this->resourceId);
+        return isset($curlResponses[$key])? $curlResponses[$key]: null;
+    }
 }
